e49fd9d0329f0ecfd42563d473c7aa8d4da455be
eduarda.rosa <eduarda.rosa@jsl.com.br>
2021-06-15 22:25:16 -0300

    Trabalhando com foreach e arquivos estaticos no express node.js

--- index.js
+++ index.js
@@ -3,17 +3,26 @@ const express = require('express');
 const app = express();
 //Setando que o motor de HTML será a EJS
 app.set('view engine', 'ejs');
+//Setando para aceitar a pasta dos conteudos estaticos.
+app.use(express.static('public'));
 
 app.get("/:nome/:lang",(req, res)=>{
     var nome =  req.params.nome; 
     var lang = req.params.lang;
-    var exibirMensagem = true;
+    var exibirMensagem = false;
+    var produtos = [
+        {nome: "Ferro Roche", preco: "17.99"},
+        {nome: "Danone", preco:"5.00"},
+        {nome: "Mussarela Real", preco:"39.99"}
+    ];
+
     res.render('index', 
     {
      nome: nome ,
      lang: lang,
      profissao: 'Developer', 
-     msg: exibirMensagem
+     msg: exibirMensagem,
+     produtos:produtos   
     });
 });
 
